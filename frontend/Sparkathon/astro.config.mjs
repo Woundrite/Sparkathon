0e0fe2973971bc6adb3297cfef625c1b628cf07d
Avinash <avinashrajput2003@gmail.com>
2025-07-12 15:56:24 +0530

    navbar

--- frontend/Sparkathon/astro.config.mjs
+++ frontend/Sparkathon/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
+// const { heroui } = require("@heroui/react");
 
 // https://astro.build/config
 export default defineConfig({
